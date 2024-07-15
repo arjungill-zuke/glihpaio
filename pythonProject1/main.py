5ad1cc36330706a236363402addfaeb9960143c5
arjungill-zuke <130119603+arjungill-zuke@users.noreply.github.com>
2024-07-14 19:47:01 -0700

    Update main.py

--- pythonProject1/main.py
+++ pythonProject1/main.py
@@ -1,9 +1,8 @@
+
 import streamlit as st
 import pandas as pd
 import requests
 from io import BytesIO
-from PIL import Image
-import base64
 
 # flask --app api.py run --port=5000
 prediction_endpoint = "http://127.0.0.1:5000/predict"
@@ -17,23 +16,6 @@ uploaded_file = st.file_uploader(
 
 # Text input for sentiment prediction
 user_input = st.text_input("Enter text and click on Predict", "")
-
-# Function to display graphs
-def display_graphs(response):
-    if 'X-Graph-Data-0' in response.headers:
-        graph_data1 = response.headers['X-Graph-Data-0']
-        graph_data2 = response.headers['X-Graph-Data-1']
-        graph_data3 = response.headers['X-Graph-Data-2']
-
-        graph_urls = [
-            "data:image/png;base64," + graph_data1,
-            "data:image/png;base64," + graph_data2,
-            "data:image/png;base64," + graph_data3,
-        ]
-
-        for graph_url in graph_urls:
-            graph_image = Image.open(BytesIO(base64.b64decode(graph_url.split(",")[1])))
-            st.image(graph_image, use_column_width=True)
 
 # Prediction on single sentence
 if st.button("Predict"):
@@ -49,7 +31,6 @@ if st.button("Predict"):
             file_name="Predictions.csv",
             key="result_download_button",
         )
-        display_graphs(response)
 
     else:
         response = requests.post(prediction_endpoint, data={"text": user_input})
